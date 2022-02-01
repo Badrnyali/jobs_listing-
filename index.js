a2e085941157b64ec556dfb00857047869751090
Badrnyali <71897147+Badrnyali@users.noreply.github.com>
2022-02-01 23:59:48 +0100

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const data = [
   {
     "id": 1,
     "company": "Photosnap",
-    "logo": "/images/photosnap.svg",
+    "logo": "images/photosnap.svg",
     "new": true,
     "featured": true,
     "position": "Senior Frontend Developer",
@@ -21,7 +21,7 @@ const data = [
   {
     "id": 2,
     "company": "Manage",
-    "logo": "/images/manage.svg",
+    "logo": "images/manage.svg",
     "new": true,
     "featured": true,
     "position": "Fullstack Developer",
@@ -36,7 +36,7 @@ const data = [
   {
     "id": 3,
     "company": "Account",
-    "logo": "/images/account.svg",
+    "logo": "images/account.svg",
     "new": true,
     "featured": false,
     "position": "Junior Frontend Developer",
@@ -51,7 +51,7 @@ const data = [
   {
     "id": 4,
     "company": "MyHome",
-    "logo": "/images/myhome.svg",
+    "logo": "images/myhome.svg",
     "new": false,
     "featured": false,
     "position": "Junior Frontend Developer",
@@ -66,7 +66,7 @@ const data = [
   {
     "id": 5,
     "company": "Loop Studios",
-    "logo": "/images/loop-studios.svg",
+    "logo": "images/loop-studios.svg",
     "new": false,
     "featured": false,
     "position": "Software Engineer",
@@ -81,7 +81,7 @@ const data = [
   {
     "id": 6,
     "company": "FaceIt",
-    "logo": "/images/faceit.svg",
+    "logo": "images/faceit.svg",
     "new": false,
     "featured": false,
     "position": "Junior Backend Developer",
@@ -96,7 +96,7 @@ const data = [
   {
     "id": 7,
     "company": "Shortly",
-    "logo": "/images/shortly.svg",
+    "logo": "images/shortly.svg",
     "new": false,
     "featured": false,
     "position": "Junior Developer",
@@ -111,7 +111,7 @@ const data = [
   {
     "id": 8,
     "company": "Insure",
-    "logo": "/images/insure.svg",
+    "logo": "images/insure.svg",
     "new": false,
     "featured": false,
     "position": "Junior Frontend Developer",
@@ -126,7 +126,7 @@ const data = [
   {
     "id": 9,
     "company": "Eyecam Co.",
-    "logo": "/images/eyecam-co.svg",
+    "logo": "images/eyecam-co.svg",
     "new": false,
     "featured": false,
     "position": "Full Stack Engineer",
@@ -141,7 +141,7 @@ const data = [
   {
     "id": 10,
     "company": "The Air Filter Company",
-    "logo": "/images/the-air-filter-company.svg",
+    "logo": "images/the-air-filter-company.svg",
     "new": false,
     "featured": false,
     "position": "Front-end Dev",
